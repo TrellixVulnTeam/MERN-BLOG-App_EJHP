d57c636c104ec2549d757e2feeddc55febef5b8f
Safdar <safdar.hussain@devorbis.com>
2022-03-25 15:59:30 +0500

    first

---
+++ Documents/perkforce-beta/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// import ReactDOM from 'react-dom';
+
+const App = ()=>{ 
+    return(
+        <>
+  <div className="container">
+            <div className="header">
+                <h1>Welcome to React application</h1>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default App;
+
+// ReactDOM.render(<h1>hello world</h1>, document.getElementById("root"))
